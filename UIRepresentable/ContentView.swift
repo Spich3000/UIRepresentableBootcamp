dc2ffeacc08f38f9ea80f91a665b5c0958cc6ac2
Spich <spichakovda@gmail.com>
2022-11-10 21:22:12 +0300

    Initial Commit

---
+++ UIRepresentable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIRepresentable
+//
+//  Created by Дмитрий Спичаков on 10.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIRepresentable/UIRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIRepresentableApp.swift
+//  UIRepresentable
+//
+//  Created by Дмитрий Спичаков on 10.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
